623e0c015b6878da50e2a6311d8cac11169dcf98
Luca Gemelli <luccagemelli@hotmail.com>
2020-07-19 00:29:35 -0300

    - Estrutura Inicial do Projeto.

---
+++ romans/src/main/java/br/com/dell/romans/RomansApplication.java
@@ -0,0 +1,13 @@
+package br.com.dell.romans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomansApplication.class, args);
+	}
+
+}
